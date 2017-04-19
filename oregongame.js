1f8adbbc9abad284db73f598a8aec0d64f46e57a
seanimamo <seanimamo@gmail.com>
2017-04-18 21:03:31 -0400

    Created the shell for the Main Menu & "Turn sound off" button

--- oregongame.js
+++ oregongame.js
@@ -1,12 +1,31 @@
-var player1name, player2name, player3name, player4name;
+		
+		
+window.onload = function() { document.getElementById("audio_MenuTheme").play();}
 
-
-/***********************Initialization*****************/
-
-function welcome(){
-
+//Functionality for sfx sound on hover menu items
+function playMenuHoverSound()
+{
+	var paperCrumble = document.getElementById("audio_menuSelect");
+	paperCrumble.play();
 }
-
-function getName(){
-
+function stopMenuHoverSound()
+{
+	var paperCrumble = document.getElementById("audio_menuSelect");
+	paperCrumble.pause();
+	paperCrumble.currentTime = 0;
+}
+//Functionality for the Turn Off/On Menu Option.
+function toggleMusic(){ 
+	var audio = document.getElementById("audio_MenuTheme");
+	var button = document.getElementById("button_toggleMusic");
+	if(button.innerHTML == "Turn sound off")
+	{
+		audio.pause();
+		button.innerHTML = "Turn sound on";
+	}
+	else
+	{
+		audio.play();
+		button.innerHTML = "Turn sound off";
+	}	
 }
